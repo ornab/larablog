094dc5da849fff5c17e3631d41c8802ef23db419
ornab <hzamil17@gmail.com>
2017-07-04 17:05:33 +0600

    modified

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -67,6 +67,10 @@
 
     </div>
     <!-- end of .container -->
+    
+    <hr>
+    
+     <p class="text-center"> Copyright@ornab - All Rights Reserved </p>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
